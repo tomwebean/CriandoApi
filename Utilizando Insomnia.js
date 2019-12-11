7910b957acd9137f98f3068223e402eb10306fa7
Wellington Bernardes <webean@outlook.com>
2019-12-11 11:52:47 -0200

    Utilizando Insomnia

---
+++ Utilizando Insomnia.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const server = express();
+
+const users = ['Diego', 'Cláudio', 'Vitor'];
+
+server.get('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  return res.json(users[index]);
+})
+
+server.listen(3000);
