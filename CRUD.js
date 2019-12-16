9afaa2bf98d1fa38c5dfa1cd4b85daa966ddc65b
Wellington Bernardes <webean@outlook.com>
2019-12-16 12:33:29 -0200

    Comentarios

--- CRUD.js
+++ CRUD.js
@@ -6,3 +6,6 @@ server.use(express.json());
 
 // Query params = ?teste=1
 // Route params = /users/1
+// Request body = { "name": "Diego", "email": "diego@rocketseat.com.br" }
+
+// CRUD - Create, Read, Update, Delete
