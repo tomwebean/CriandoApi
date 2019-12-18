12753052e4d5aee65be83c74590a25f9cba5bf53
Wellington Bernardes <webean@outlook.com>
2019-12-18 13:48:27 -0200

    StpL 30

--- CRUD.js
+++ CRUD.js
@@ -20,3 +20,11 @@ server.user((req, res, next) => {
 
   console.timeEnd('Request');
 });
+
+function checkUserExists(req, res, next) {
+  if (!req,body.name) {
+    return res.status(400).json({ error: 'User name is required' });
+  }
+
+  return next();
+}
